8b8bb4b98cd4f5f7ba54080be304b88e5e8e77d5
Ryan Clark <ryanc@accentservices.com>
2024-08-24 15:59:29 -0500

    chore: Update default.nix and settings.nix

--- config/default.nix
+++ config/default.nix
@@ -1,32 +1,32 @@
 _: {
   imports = [
     # General Configuration
-    ./settings.nix
-    ./keymaps.nix
     ./auto_cmds.nix
     ./file_types.nix
+    ./keymaps.nix
+    ./settings.nix
 
     # Themes
     ./plugins/themes/default.nix
 
     # Completion
-    ./plugins/cmp/cmp.nix
-    ./plugins/cmp/cmp-copilot.nix
-    ./plugins/cmp/lspkind.nix
     ./plugins/cmp/autopairs.nix
+    ./plugins/cmp/cmp-copilot.nix
+    ./plugins/cmp/cmp.nix
+    ./plugins/cmp/lspkind.nix
 
     # Snippets
     ./plugins/snippets/luasnip.nix
 
     # Editor plugins and configurations
-    ./plugins/editor/neo-tree.nix
-    ./plugins/editor/treesitter.nix
-    ./plugins/editor/undotree.nix
+    ./plugins/editor/copilot-chat.nix
     ./plugins/editor/illuminate.nix
     ./plugins/editor/indent-blankline.nix
-    ./plugins/editor/todo-comments.nix
-    ./plugins/editor/copilot-chat.nix
     ./plugins/editor/navic.nix
+    ./plugins/editor/neo-tree.nix
+    ./plugins/editor/todo-comments.nix
+    ./plugins/editor/treesitter.nix
+    ./plugins/editor/undotree.nix
 
     # UI plugins
     ./plugins/ui/bufferline.nix
@@ -34,23 +34,23 @@ _: {
     ./plugins/ui/startup.nix
 
     # LSP and formatting
-    ./plugins/lsp/lsp.nix
     ./plugins/lsp/conform.nix
     ./plugins/lsp/fidget.nix
+    ./plugins/lsp/lsp.nix
 
     # Git
-    ./plugins/git/lazygit.nix
     ./plugins/git/gitsigns.nix
+    ./plugins/git/lazygit.nix
 
     # Utils
     ./plugins/utils/colorizer.nix
-    ./plugins/utils/telescope.nix
-    ./plugins/utils/whichkey.nix
     ./plugins/utils/extra_plugins.nix
-    ./plugins/utils/mini.nix
     ./plugins/utils/markdown-preview.nix
+    ./plugins/utils/mini.nix
     ./plugins/utils/obsidian.nix
+    ./plugins/utils/telescope.nix
     ./plugins/utils/toggleterm.nix
+    ./plugins/utils/whichkey.nix
     ./plugins/utils/yazi.nix
   ];
 }
