7f0b03e1df7035b1003e49ab9c08840ebde99f02
Ryan Clark <ryanc@accentservices.com>
2024-08-25 10:23:57 -0500

    chore: Update neo-tree plugin configuration
    This commit updates the configuration of the neo-tree plugin. It enables the `addBlankLineAtTop` option and adds the `enableGitStatus` and `enableModifiedMarkers` options. The order of the `sources` array is also modified. Additionally, the `filesystem` component is updated with new options such as `asyncDirectoryScan`, `filteredItems`, `scanMode`, `searchLimit`, and `window.mappings`.
    
    ```

--- config/plugins/editor/neo-tree.nix
+++ config/plugins/editor/neo-tree.nix
@@ -1,16 +1,15 @@
 {
   plugins.neo-tree = {
     enable = true;
-    sources = ["filesystem" "buffers" "git_status" "document_symbols"];
-    addBlankLineAtTop = false;
-
-    filesystem = {
-      bindToCwd = false;
-      followCurrentFile = {
-        enabled = true;
-      };
-    };
-
+    addBlankLineAtTop = true;
+    enableGitStatus = true;
+    enableModifiedMarkers = true;
+    sources = [
+      "filesystem"
+      "buffers"
+      "git_status"
+      "document_symbols"
+    ];
     defaultComponentConfigs = {
       indent = {
         withExpanders = true;
@@ -18,7 +17,6 @@
         expanderExpanded = " ";
         expanderHighlight = "NeoTreeExpander";
       };
-
       gitStatus = {
         symbols = {
           added = " ";
@@ -31,6 +29,45 @@
           unstaged = "";
           untracked = "";
         };
+      };
+    };
+    filesystem = {
+      asyncDirectoryScan = "auto";
+      bindToCwd = false;
+      followCurrentFile = {
+        enabled = true;
+      };
+      filteredItems = {
+        alwaysShow = [
+          ".gitignore"
+        ];
+        hideByName = [
+          ".DS_Store"
+          ".git"
+          ".hg"
+          ".svn"
+          ".vscode"
+          "node_modules"
+          "vendor"
+        ];
+
+      };
+      scanMode = "shallow";
+      searchLimit = 50;
+      useLibuvFileWatcher = false;
+      window.mappings = {
+        H = "toggle_hidden";
+        "/" = "fuzzy_finder";
+        D = "fuzzy_finder_directory";
+        # "/" = "filter_as_you_type"; # this was the default until v1.28
+        "#" = "fuzzy_sorter"; # fuzzy sorting using the fzy algorithm
+        # D = "fuzzy_sorter_directory";
+        f = "filter_on_submit";
+        "<C-x>" = "clear_filter";
+        "<bs>" = "navigate_up";
+        "." = "set_root";
+        "[g" = "prev_git_modified";
+        "]g" = "next_git_modified";
       };
     };
   };
