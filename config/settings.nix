1454a12490fdc317e692b2579f20df4097b22742
Ryan Clark <ryanc@accentservices.com>
2024-08-25 11:45:10 -0500

    chore: Update settings.nix file with improved editor configurations
    This commit updates the settings.nix file to include improved editor configurations. The changes include:
    - Faster completion with the 'updatetime' option set to 100
    - Hidden closed buffers kept open in the background with the 'hidden' option
    - Enabled mouse control with the 'mouse' option set to "a"
    - Display of absolute line numbers with the 'number' option
    - Relative line numbers with the 'relativenumber' option
    - New window placement options with the 'splitbelow' and 'splitright' options
    - Disabled swap file with the 'swapfile' option
    - Modeline tags with the 'modeline' option
    - Undo history saved and restored automatically with the 'undofile' option
    - Incremental search with the 'incsearch' option
    - Case-insensitive search with the 'ignorecase' and 'smartcase' options
    - Highlighting of the screen line of the cursor with the 'cursorline' option
    - Highlighting of the screen column of the cursor disabled with the 'cursorcolumn' option
    - Signcolumn shown with the 'signcolumn' option
    - Columns highlighted with the 'colorcolumn' option
    - Status line used for the last window with the 'laststatus' option
    - File-content encoding set to UTF-8 with the 'fileencoding' option
    - 24-bit RGB color enabled with the 'termguicolors' option
    - Spell check enabled with the 'spell' option
    - Text wrapping disabled with the 'wrap' option
    
    These changes improve the overall editor experience and customization options.

--- config/settings.nix
+++ config/settings.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  lib,
+  pkgs,
+  ...
+}:
+
 {
   config = {
     extraConfigLuaPre =
@@ -18,15 +23,36 @@
     };
 
     opts = {
+      updatetime = 100; # Faster completion
 
       # Line numbers
-      number = true;
-      relativenumber = true;
-      hidden = true;
-      mouse = "a";
-      mousemodel = "extend";
-      splitbelow = true;
-      splitright = true;
+      hidden = true; # Keep closed buffer open in the background
+      mouse = "a"; # Enable mouse control
+      mousemodel = "extend"; # Mouse right-click extends the current selection
+      number = true; # Display the absolute line number of the current line
+      relativenumber = true; # Relative line numbers
+      splitbelow = true; # A new window is put below the current one
+      splitright = true; # A new window is put right of the current one
+
+      swapfile = false; # Disable the swap file
+      modeline = true; # Tags such as 'vim:ft=sh'
+      modelines = 100; # Sets the type of modelines
+      undofile = true; # Automatically save and restore undo history
+      incsearch = true; # Incremental search: show match for partly typed search command
+      ignorecase = true; # When the search query is lower-case, match both lower and upper-case
+      #   patterns
+      smartcase = true; # Override the 'ignorecase' option if the search pattern contains upper
+      #   case characters
+      cursorline = true; # Highlight the screen line of the cursor
+      cursorcolumn = false; # Highlight the screen column of the cursor
+      signcolumn = "yes"; # Whether to show the signcolumn
+      colorcolumn = "100"; # Columns to highlight
+      laststatus = 3; # When to use a status line for the last window
+      fileencoding = "utf-8"; # File-content encoding for the current buffer
+      termguicolors = true; # Enables 24-bit RGB color in the |TUI|
+      spelllang = lib.mkDefault [ "en_us" ]; # Spell check languages
+      spell = true; # Highlight spelling mistakes (local to window)
+      wrap = false; # Prevent text from wrapping
 
       # Tab Options
       tabstop = 2;
@@ -36,21 +62,59 @@
       smartindent = true;
 
       # Folding
-      foldmethod = "manual";
-      foldenable = false;
+      foldlevel = 99; # Folds with a level higher than this number will be closed
+      foldcolumn = "1";
+      foldenable = true;
+      foldlevelstart = -1;
+      fillchars = {
+        horiz = "━";
+        horizup = "┻";
+        horizdown = "┳";
+        vert = "┃";
+        vertleft = "┫";
+        vertright = "┣";
+        verthoriz = "╋";
 
+        eob = " ";
+        diff = "╱";
+
+        fold = " ";
+        foldopen = "";
+        foldclose = "";
+
+        msgsep = "‾";
+      };
+
+      # backspace = { append = [ "nostop" ]; };
+      breakindent = true;
+      cmdheight = 0;
+      copyindent = true;
+      # diffopt = { append = [ "algorithm:histogram" "linematch:60" ]; };
+      # fillchars = { eob = " "; };
+      history = 100;
+      infercase = true;
+      linebreak = true;
+      preserveindent = true;
+      pumheight = 10;
+      # shortmess = { append = { s = true; I = true; }; };
+      showmode = false;
+      showtabline = 2;
+      timeoutlen = 500;
+      title = true;
+      # viewoptions = { remove = [ "curdir" ]; };
+      virtualedit = "block";
+      writebackup = false;
+
+      lazyredraw = false; # Faster scrolling if enabled, breaks noice
+      synmaxcol = 240; # Max column for syntax highlight
+      showmatch = true; # when closing a bracket, briefly flash the matching one
+      matchtime = 1; # duration of that flashing n deci-seconds
+      startofline = true; # motions like "G" also move to the first char
+      report = 9001; # disable "x more/fewer lines" messages
 
       clipboard = "unnamedplus";
-      showtabline = 2;
-      breakindent = true;
-      cursorline = true;
+
       scrolloff = 8;
-      linebreak = true;
-      spell = false;
-      swapfile = false;
-      timeoutlen = 300;
-      termguicolors = true;
-      showmode = false;
       splitkeep = "screen";
     };
   };
